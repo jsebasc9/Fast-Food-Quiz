e884c628dc6d975a3c51826aec0cdcf73f280cba
Juan Sebastian Calderon <juans.jc2@hotmail.com>
2021-09-28 15:03:33 +0800

    Correcting something and int func step 6 working

--- Testing.py
+++ Testing.py
@@ -1,5 +1,8 @@
-users = ["Test User", "Real User 1", "Real User 2","Test User", "Real User 2"]
-for index, user in enumerate(users):
-    if index == 0:
-        print("Extra verbose output for:", user)
-    print(user)
+ # repeatedly re-prompt the user for valid input no whitespace
+        fast_food['name'] = input_something(fast_food['name']) # repeatedly re-prompt the user for valid input no whitespace  
+        fast_food['energy'] = input_int(fast_food['energy']) # repeatedly re-prompt the user for valid input integer
+        fast_food['fat'] = input_int(fast_food['fat']) # repeatedly re-prompt the user for valid input integer
+        fast_food['protein'] = input_int(fast_food['protein']) # repeatedly re-prompt the user for valid input integer
+        fast_food['carbohydrates'] = input_int(fast_food['carbohydrates']) # repeatedly re-prompt the user for valid input integer
+        fast_food['sugars'] = input_int(fast_food['sugars']) # repeatedly re-prompt the user for valid input integer
+        fast_food['sodium'] = input_int(fast_food['sodium']) # repeatedly re-prompt the user for valid input integer
--- admin.py
+++ admin.py
@@ -23,11 +23,11 @@ def input_int(prompt):
             if response <= 0:
                 prompt = input('Invalid input - Try again: ')
             else:
+                return response
                 break
         except ValueError:
             prompt = input('Invalid input - Try again: ')
             continue
-        return response
     # pass
 
 
@@ -39,6 +39,7 @@ def input_something(prompt):
         if not prompt.strip():
             prompt = input('Invalid input - Try again: ')
         else:
+            return prompt
             break
     # pass
 
@@ -85,7 +86,7 @@ while True:
         
         # repeatedly re-prompt the user for valid input no whitespace
         fast_food['name'] = input('Enter name of fast-food item: ')
-        input_something(fast_food['name']) # repeatedly re-prompt the user for valid input no whitespace         
+        fast_food['name'] = input_something(fast_food['name']) # repeatedly re-prompt the user for valid input no whitespace         
         fast_food['energy'] = input('Enter energy in kilojoules: ')
         input_int(fast_food['energy']) # repeatedly re-prompt the user for valid input integer
         fast_food['fat'] = input('Enter fat in grams: ')
@@ -127,7 +128,8 @@ while True:
         flag_search = False # Defining flag to confirm available results
         index = 1 # Initialization of index for search results
         term = input('Enter search term: ')
-        input_something(term) # repeatedly re-prompt the user for valid input no whitespace
+        term = input_something(term) # repeatedly re-prompt the user for valid input no whitespace
+        print(term)
         print('Search results: ')
         if data:
             for itemlist in data: # Iterating data dictionary using enumerate to start from 1
@@ -148,8 +150,43 @@ while True:
 
 
 
-    # elif choice == 'v':
+    elif choice == 'v':
         # View a fast-food item.
+        number_item = input('Enter item number to view: ')
+        number_item = input_int(number_item) # repeatedly re-prompt the user for valid input integer
+        if data:
+                for index, itemlist in enumerate(data, 1): # Iterating data dictionary using enumerate to start from 1
+                    temp_dictionary = itemlist # Setting item as a Dictionary to iterate
+                    if int(number_item) <= len(data): # checking if index number entered does not exist
+                        if int(number_item) == index: # checking if index is equal to number_item to print information
+                            for itemdictionary, detail in temp_dictionary.items(): # Iterating dictionary
+                                if itemdictionary == 'name': # Looking for key 'name'
+                                    name = detail # Setting name to display depending of key
+                                    print(name, sep='', end='\n')
+                                if itemdictionary == 'energy': # Looking for key 'name' 
+                                    name = detail
+                                    number_calories = int(name) / 4.184
+                                    print('  Energy: ', name, ' kilojules (', round(number_calories),')',sep='', end='\n')
+                                if itemdictionary == 'fat': # Looking for key 'name'
+                                    name = detail
+                                    print('  Fat: ', name, ' grams', sep='', end='\n')
+                                if itemdictionary == 'protein': # Looking for key 'name'
+                                    name = detail
+                                    print('  Protein: ', name, ' grams',sep='', end='\n')
+                                if itemdictionary == 'carbohydrates': # Looking for key 'name'
+                                    name = detail
+                                    print('  Carbohydrates: ', name, ' grams',sep='', end='\n')
+                                if itemdictionary == 'sugars': # Looking for key 'name'
+                                    name = detail
+                                    print('  Sugars: ', name, ' grams',sep='', end='\n')
+                                if itemdictionary == 'sodium': # Looking for key 'name'
+                                    name = detail
+                                    print('  Sodium: ', name, ' milligrams',sep='', end='\n')
+                    else:
+                        print('Invalid index number') 
+                        break    
+        else:
+            print('No items saved') 
         # See Point 6 of the "Requirements of admin.py" section of the assignment brief.
         # pass
 
